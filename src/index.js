b0ea91a689df5c66a6abc0638843353b0738ca1f
kleviskl <kleviskoleci29@gmail.com>
2021-10-21 14:40:44 +0200

    jwt hash password and hide the uneccesary data of the user on the model toJSON method to the object

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,14 +26,14 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
-    const password = 'Red12345'
-    const hashPassword = await bcrypt.hash(password, 8)
+    const token = jwt.sign({ _id: 'abc123' }, 'thisisnodejsprogrammer', { expiresIn: '7 days' })
+    console.log(token)
 
-    console.log(password)
-    console.log(hashPassword)
+    const data = jwt.verify(token, 'thisisnodejsprogrammer')
+    console.log(data)
 }
 
 myFunction()
--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         trim: true
-    }, 
+    },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('EMail is invalid')
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
             }
         }
     },
@@ -24,9 +26,9 @@ const userSchema = new mongoose.Schema({
         required: true,
         minlength: 7,
         trim: true,
-        validate(value){
-            if (value.toLowerCase().includes('password')){
-                throw new Error('Passowrd cannot contain "password"')
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
             }
         }
     },
@@ -34,13 +36,56 @@ const userSchema = new mongoose.Schema({
         type: Number,
         default: 0,
         validate(value) {
-            if(value <0){
-                throw new Error('Age must be a positive number')
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisisnodejsprogrammer')
+   
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    
+    return token
+}
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
 
